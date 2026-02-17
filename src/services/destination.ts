862efad17b2e743fb74acfd179b168833ed50633
Mina R <minarumang@icloud.com>
2026-02-17 21:57:48 +0100

    add getDestinationInfo function

--- src/services/destination.ts
+++ src/services/destination.ts
@@ -1,15 +1,57 @@
 // Async API Fetch
-export const getDestinationInfo = async (countryName: string) => {
+
+type Currency = {
+  // if we want to add another key value pair I do it here
+  symbol: string;
+  name: string;
+};
+
+type DestinationInfo = {
+  //or here depending on currency or destination info
+  currency: Currency;
+  flag: string;
+};
+
+type RestCountryResponse = {
+  // defined RestCountryRespone to avoid implicit any
+  currencies: Record<string, unknown>;
+  flag: string;
+};
+
+// main function: fetch cpuntry info from rest
+export const getDestinationInfo = async (
+  countryName: string,
+): Promise<DestinationInfo> => {
+  // this function returns a Promise that resolves to DestinationInfo
   try {
-    const response = await fetch(
-      `https://restcountries.com/v3.1/name/${countryName}`,
-    );
-    const data = await response.json();
+    // here we fetch
+    const url = `https://restcountries.com/v3.1/name/${countryName}`;
+    const response: Response = await fetch(url);
+    if (!response.ok) {
+      // to prevent run time crashes on failed requests
+      throw new Error(`Failed to fetch destination info for url: ${url}`);
+    }
+    const data: RestCountryResponse[] = await response.json();
+    // currencies = data[0].currencies[0];
+    const currency = Object.values(data[0]?.currencies)[0];
+    console.log(data[0]?.flag); // try to find another value here if needed (flag)
+
+    console.log();
     return {
-      currency: Object.keys(data[0].currencies)[0],
+      //another value if needed add here
+      currency: {
+        symbol: currency?.symbol,
+        name: currency?.name,
+      },
       flag: data[0].flag,
     };
-  } catch (error) {
-    throw new Error("could not fetch country data");
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(`destination error: ${error.message}`);
+    }
+    throw new Error("unknown error");
   }
 };
+
+const destInfo = await getDestinationInfo("sweden");
+console.log(destInfo);
