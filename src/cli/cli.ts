8dc5f437c34b6688777d657bf486a9f6fa4c79d7
Rut <rutwintzell@outlook.com>
2026-02-18 19:56:04 +0100

    Added handleBudgetMenu function

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,31 @@
 // Imports
 import inquirer from "inquirer";
-// import { calculateTotalCost, getHighCostActivities, getActivitiesWithinBudget, getBudgetSummary } from "../services/budget.js";
+import {
+  calculateTotalCost,
+  getHighCostActivities,
+  getActivitiesWithinBudget,
+  getBudgetSummary,
+} from "../services/budget.js";
+import type { Trip } from "../models/trip.js";
+
+const trips = [] = []; // ADD TRIPS LATER
+
+const handleBudgetMenu = async (selectedTrip: Trip) => {
+  const { budgetAction } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "budgetAction",
+      message: `Budget Management for ${selectedTrip.destination}:`,
+      choices: [
+        "View Total Cost",
+        "Filter High Cost Activities",
+        "Filter Activities Within Budget",
+        "Back to Main Menu",
+      ],
+    },
+  ]);
+
+// Switch case for budget actions here 
 
 // Menu Loop
 const mainMenu = async () => {
