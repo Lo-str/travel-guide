d83c2cb46deb91d8dd19714a887fea0fdb24f288
LoS <lostreit@pm.me>
2026-02-18 16:35:52 +0100

    feat: add Trip and Activity models with CRUD logic

--- src/models/activity.ts
+++ src/models/activity.ts
@@ -2,7 +2,7 @@
 export type Activity = {
   id: string;
   name: string;
-  cost: number;
-  category: "food" | "transport" | "sightseeing";
+  cost?: number;
+  category?: "food" | "transport" | "sightseeing";
   startTime: Date;
 };
