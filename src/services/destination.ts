6784510cb715e8e47986413036ea2a987a98840d
Mina R <minarumang@icloud.com>
2026-02-19 22:40:05 +0100

    fix typing add comments

--- src/services/destination.ts
+++ src/services/destination.ts
@@ -27,28 +27,41 @@ type RestCountryResponse = {
 export const getDestinationInfo = async (
   countryName: string,
 ): Promise<DestinationInfo> => {
-  // this function returns a Promise that resolves to DestinationInfo
   try {
     // here we fetch
-    const url = `https://restcountries.com/v3.1/name/${countryName}`;
-    const response: Response = await fetch(url);
+    const url = `https://restcountries.com/v3.1/name/${countryName}`; // Define url for the API
+    const response: Response = await fetch(url); // Here we fetch data from API
     if (!response.ok) {
-      // to prevent run time crashes on failed requests
+      // We check if the response is OK, if not OK, we throw a new Error.
       throw new Error(`Failed to fetch destination info for url: ${url}`);
     }
-    const data: RestCountryResponse[] = await response.json();
-    // currencies = data[0].currencies[0];
-    const currency = Object.values(data[0]?.currencies)[0];
-    console.log(data[0]?.flag); // try to find another value here if needed (flag)
+    const data: RestCountryResponse[] = await response.json(); // Here we define a new list (JSON representation of the data)
+    const destination_info_data = data[0]; // The list contains the interesting data as the first element, we take this.
 
-    console.log();
+    if (!destination_info_data) {
+      // If we get nothing from data[0], then we throw new error since the list is empty.
+      throw new Error(`No country found for ${countryName}`);
+    }
+
+    if (!destination_info_data.currencies) {
+      // We check if currency exist in data[0] (data[0] is same as destination_info_data)
+      throw new Error("Currency information not available");
+    }
+
+    if (Object.keys(destination_info_data.currencies).length === 0) {
+      // We check if currencies contain any keys. Otherwise we raise error since we won't find symbol or name later.
+      throw new Error("Currency information not available");
+    }
+
+    const currency = Object.values(destination_info_data.currencies)[0]!; // Then we take the values, i.e., { "symbol": "kr", "name": "Swedish krona" }
+    //console.log(data[0]?.flag); // try to find another value here if needed (flag)
     return {
       //another value if needed add here
       currency: {
-        symbol: currency?.symbol,
-        name: currency?.name,
+        symbol: currency.symbol,
+        name: currency.name,
       },
-      flag: data[0].flag,
+      flag: destination_info_data.flag,
     };
   } catch (error: unknown) {
     if (error instanceof Error) {
@@ -58,5 +71,5 @@ export const getDestinationInfo = async (
   }
 };
 
-const destInfo = await getDestinationInfo("sweden");
+const destInfo = await getDestinationInfo("japan");
 console.log(destInfo);
